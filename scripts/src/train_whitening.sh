8dc976aa748663643ab950487c8d3246d0dca053
Karthik Singaravadivelan <karthiksing05@gmail.com>
2025-07-19 08:41:41 -0400

    small change to train_whitening.sh

--- scripts/src/train_whitening.sh
+++ scripts/src/train_whitening.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH --job-name=train_whitening
-#SBATCH --time=00:03:00
+#SBATCH --time=00:00:00
 #SBATCH --output=/nethome/ksingara3/slurm_outputs/train_whitening.out
 #SBATCH --error=/nethome/ksingara3/slurm_errors/train_whitening.err
-#SBATCH --partition=debug
+#SBATCH --partition="tail-lab"
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-task=6
 #SBATCH --qos="short"
 #SBATCH --exclude="clippy"
 
